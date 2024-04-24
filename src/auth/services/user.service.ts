bcce2a0fadeb7933d7a7fe773538d383d546a42a
seraphicblue <ggj05037@gmail.com>
2024-04-24 22:44:40 +0900

    회원가입

--- src/auth/services/user.service.ts
+++ src/auth/services/user.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { UserRepository } from '../repository';
+import * as argon2 from 'argon2';
+import { CreateUserDto } from '../dto/CreateUserDto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private readonly logger = new Logger(UserService.name);
+
+  constructor(private readonly userRepo: UserRepository) {}
+  async createUser(dto: CreateUserDto): Promise<User | null> {
+    const user = await this.userRepo.findOneByEmail(dto.email);
+    if (user) {
+      this.logger.error(`${dto.email} already exists`);
+      return null; // 에러 상황을 나타내는 null 반환
+    }
+    const hashedPassword = await argon2.hash(dto.password);
+    return this.userRepo.createUser(dto, hashedPassword);
+  }
+}
