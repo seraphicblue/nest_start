c6f0ce4346ebd7ffbc1305cf119ba3ed30d9eee7
seraphicblue <ggj05037@gmail.com>
2024-05-01 23:37:44 +0900

    로그인 app모듈 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { User } from './auth/entities/user.entity';
-import { AccessToken } from 'auth/entities/access-token.entity';
-import { RefreshToken } from 'auth/entities/refresh-token.entity';
-import { AccessLog } from 'auth/entities/access-log.entity';
-import { TokenBlacklist } from 'auth/entities/token-blacklist.entity';
+import { AccessToken } from './auth/entities/access-token.entity';
+import { RefreshToken } from './auth/entities/refresh-token.entity';
+import { AccessLog } from './auth/entities/access-log.entity';
+import { TokenBlacklist } from './auth/entities/token-blacklist.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { validationSchema } from 'config/validation.schema';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema,
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'user',
-        password: 'password',
-        database: 'devcamp',
+        host: configService.get('DB_HOST', 'localhost'),
+        port: configService.get('DB_PORT', 5432),
+        username: configService.get('DB_USERNAME', 'user'),
+        password: configService.get('DB_PASSWORD', 'password'),
+        database: configService.get('DB_DATABASE', 'devcamp'),
         entities: [User, AccessToken, RefreshToken, AccessLog, TokenBlacklist],
-        synchronize: true,
+        synchronize: configService.get('DB_SYNCHRONIZE', true),
       }),
+      inject: [ConfigService],
     }),
     AuthModule,
   ],
